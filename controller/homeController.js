944d5a261931ce17e6df771edc0fbf704debb67c
Geovanna Lessa <geovannalessa2@gmail.com>
2022-08-26 15:59:59 -0300

    Trazendo views para o ejs

---
+++ controller/homeController.js
@@ -0,0 +1,25 @@
+const paginasController = {
+    showLogin:(req,res)=>{
+        res.render('login.ejs')
+    },
+    showHome:(req, res)=>{
+        res.render('home.ejs')
+    },
+    showListagem:(req,res)=>{
+        res.render('listagemProduto.ejs')
+    },
+    showProdutoInterno:(req,res)=>{
+        res.render('produtoInterno.ejs')
+    },
+    showCarrinho:(req, res)=>{
+        res.render('carrinho.ejs')
+    },
+    showFinalizacao:(req, res)=>{
+        res.render('finalizacao.ejs')
+    },
+    showPainel:(req, res)=>{
+        res.render('painel.ejs')
+    }
+}
+
+module.exports = paginasController;
--- router/homeRouter.js
+++ router/homeRouter.js
@@ -1,4 +1,12 @@
 const express = require('express');
-const app = require('express');
-const homeController = require('express');
+const homeController = require('../controller/homeController');
+const router = express.Router();
 
+router.get('/', homeController.showHome);
+router.get('/carrinho', homeController.showCarrinho);
+router.get('/login', homeController.showLogin);
+router.get('/listagem', homeController.showListagem);
+router.get('/produto', homeController.showProdutoInterno);
+router.get('/finalizacao', homeController.showFinalizacao);
+router.get('/painel' , homeController.showPainel);
+module.exports = router
