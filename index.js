1efa8f4e5ea212aef21dc6e5a4c669bcb368e62b
Geovanna Lessa <geovannalessa2@gmail.com>
2022-08-26 16:26:28 -0300

    Adicionado o css no ejs

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Criar um servidor express (app)
 const express = require("express");
+
+const path = require('path');
 //importando o homeRouter 
 const homeRouter = require('./router/homeRouter')
 const app = express();
 //defini que a view wngine sera ejs
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 //  Fazer com que ele use o homeRouter
 app.use ('/', homeRouter);
 //
