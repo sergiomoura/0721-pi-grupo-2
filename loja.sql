43dfe13e377be75608c2341e5e3e53f2cff8eaa2
Sérgio Moura <smouracalmon@gmail.com>
2022-11-05 22:58:21 -0300

    Corrigido script de criação de banco de dados

--- loja.sql
+++ loja.sql
@@ -11,7 +11,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema langglow
 -- -----------------------------------------------------
-drop DATABASE EXISTS `langglow`;
+DROP DATABASE IF EXISTS `langglow`;
 CREATE SCHEMA IF NOT EXISTS `langglow` DEFAULT CHARACTER SET utf8 ;
 USE `langglow` ;
 
